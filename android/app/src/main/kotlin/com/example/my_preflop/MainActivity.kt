b996a551d0885651c22e41f17122ba11d51cade9
hanatani <adgjmptwhanatani@gmail.com>
2023-01-12 01:19:16 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/my_preflop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_preflop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('My Preflop'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                child: const Text("open"),
+                onPressed: () {},
+              ),
+              ElevatedButton(
+                child: const Text("vs Open"),
+                onPressed: () {},
+              ),
+              ElevatedButton(
+                child: const Text("vs 3bet"),
+                onPressed: () {},
+              ),
+              ElevatedButton(
+                child: const Text("vs 4bet"),
+                onPressed: () {},
+              ),
+              ElevatedButton(
+                child: const Text("vs 5bet"),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
